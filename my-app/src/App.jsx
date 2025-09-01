373fb07ec6293874f1f054fca4dce91eec7a4a9c
DebugWithNazi <121787737+DebugWithNazi@users.noreply.github.com>
2025-09-01 12:55:08 +0500

    Update App.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,8 +1,36 @@
 import Form from "./components/Form";
+import { act, useReducer, useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, 0);
+
+  function reducer(state, action) {
+    if (action.type === "increment") {
+      return state + action.payLoad;
+    } else if (action.type === "decrement") {
+      return state - action.payLoad;
+    } else {
+      return state;
+    }
+  }
+
   return (
     <>
+      <h2>Reducer</h2>
+
+      <div>{count}</div>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <div>{state}</div>
+      <button onClick={() => dispatch({ type: "increment", payLoad: 1 })}>
+        Increment
+      </button>
+      <button onClick={() => dispatch({ type: "decrement", payLoad: 1 })}>
+        Decrement
+      </button>
+
+      <h2>Form Component</h2>
       <div>
         <Form />
       </div>
